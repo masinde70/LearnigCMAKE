344fbe108117c237c40503024b794e3ed92b2462
masinde70 <masinde70@gmail.com>
2020-10-21 17:56:22 +0300

    path_with_boost

--- path_info.cpp
+++ path_info.cpp
@@ -4,3 +4,20 @@
 using namespace std;
 
 using namespace boost::filesystem;
+
+const char *say_what(bool b) { return b ? "true" : "false";}
+
+int main(int argc, char * argv[]){
+    if (argc < 2){
+        cout << "Usage: path_info path element [path_element....]\n";
+        "compose a path via operator/= from one or more path-element arguments\n"
+        "Example: path_info fooo/bar baz\n";
+
+        #ifdef BOOST_POSIX_API
+                    "       would report path info about the composed foo/bar/baz\n";
+        #else // BOOST_WINDOWS_API
+                    "       Would report inof about the composed path foo/bar\\baz\n";
+        #endif
+            return 1;
+    }
+}
